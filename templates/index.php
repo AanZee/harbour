85d41825221e5eacbd5fbbfe4db153ac2dbb2b03
Bart van de Biezen <bart@bartvandebiezen.com>
2015-03-24 14:21:16 +0100

    Changed header/footer to page-header/page-footer

--- templates/index.php
+++ templates/index.php
@@ -2,11 +2,11 @@
 <html lang="nl">
 	<?php $pageTitle = "home"; include("includes/head.php"); ?>
 	<body>
-		<?php include("includes/header.php"); ?>
+		<?php include("includes/page-header.php"); ?>
 
 		<p>Professional Front-end project boilerplate without the bloat</p>
 
-		<?php include("includes/footer.php"); ?>
+		<?php include("includes/page-footer.php"); ?>
 		<?php include("includes/javascript.php"); ?>
 	</body>
 </html>
