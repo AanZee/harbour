e04cf2cf2c239a43b305fd08471247a328d1bb49
Bart van de Biezen <bart@bartvandebiezen.com>
2015-03-24 11:10:51 +0100

    Made scaling possible and moved charset to top
    It is better to let users just scale the content if they want to.

--- templates/includes/head.php
+++ templates/includes/head.php
@@ -6,9 +6,9 @@ if(!$pageTitle) {
 
 ?>
 <head>
+	<meta charset="utf-8">
 	<!-- Stylesheets at the top for better performance -->
 	<link rel="stylesheet" href="/css/main.css" />
-	<meta charset="utf-8">
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
 
 	<title><?php if($pageTitle != "home") { echo $pageTitle . " &middot; "; } ?>Harbour | Front-end framework</title>
@@ -16,7 +16,7 @@ if(!$pageTitle) {
 	<meta name="description" content="Front-end framework" />
 	<meta name="robots" content="index, follow">
 
-	<meta name="viewport" content="initial-scale=1.0, maximum-scale=1, width=device-width, user-scalable=no, minimal-ui">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
 	<!-- TODO: Seperate map with all modern browser icons -->
 	<!-- <link rel="apple-touch-icon" href="/images/apple-touch-icon.png">
