ad1d8592c78ca7f5ac6ca65ae43bc9a34b436c4a
Bart van de Biezen <bart@bartvandebiezen.com>
2015-03-24 11:12:33 +0100

    Moved viewport above css

--- templates/includes/head.php
+++ templates/includes/head.php
@@ -7,7 +7,8 @@ if(!$pageTitle) {
 ?>
 <head>
 	<meta charset="utf-8">
-	<!-- Stylesheets at the top for better performance -->
+	<!-- Viewport and stylesheet at the top for better performance -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="/css/main.css" />
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
 
@@ -15,8 +16,6 @@ if(!$pageTitle) {
 
 	<meta name="description" content="Front-end framework" />
 	<meta name="robots" content="index, follow">
-
-	<meta name="viewport" content="width=device-width, initial-scale=1">
 
 	<!-- TODO: Seperate map with all modern browser icons -->
 	<!-- <link rel="apple-touch-icon" href="/images/apple-touch-icon.png">
