8aed8cc94cbebc4d952252468e8c2a19b44bfa04
Michael Maat <michael@aanzee.nl>
2018-12-28 17:03:54 +0100

    [HAR-161] Use variable for S3 URL

--- scripts/deploy-master.sh
+++ scripts/deploy-master.sh
@@ -14,10 +14,11 @@ npm install
 echo "Building website"
 JEKYLL_BASE_URL=/$PACKAGE_VERSION npm run build # TODO: remove base url
 
-echo $S3_FOLDER
+S3_BUCKET_URL=$AWS_BUCKET/$PACKAGE_VERSION
+
 echo "Deploying to S3"
 aws configure set preview.cloudfront true
-aws s3 sync ./_gh_pages/. s3://$AWS_BUCKET/$PACKAGE_VERSION --acl public-read
+aws s3 sync ./_gh_pages/. s3://$S3_BUCKET_URL --acl public-read
 
 echo "Invalidating cache"
 aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_DISTRIBUTION_ID --paths "/*" # TODO: tweak invalidation
