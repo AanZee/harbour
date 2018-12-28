ad9d0154a5f6fe76bae8734646e704da816c611d
Michael Maat <michael@aanzee.nl>
2018-12-28 16:54:38 +0100

    [HAR-161] User real version folder

--- scripts/deploy-master.sh
+++ scripts/deploy-master.sh
@@ -1,3 +1,10 @@
+echo "Setting package version variable"
+PACKAGE_VERSION=$(cat package.json \
+| grep version \
+| head -1 \
+| awk -F: '{ print $2 }' \
+| sed 's/[",]//g')
+
 echo "Installing bundle"
 bundle install
 
@@ -5,12 +12,12 @@ echo "Installing npm packages"
 npm install
 
 echo "Building website"
-JEKYLL_BASE_URL=/0.0.2 npm run build # TODO: remove base url
+JEKYLL_BASE_URL=/$PACKAGE_VERSION npm run build # TODO: remove base url
 
 echo $S3_FOLDER
 echo "Deploying to S3"
 aws configure set preview.cloudfront true
-aws s3 sync ./_gh_pages/. s3://$AWS_BUCKET/0.0.3 --acl public-read
+aws s3 sync ./_gh_pages/. s3://$AWS_BUCKET/$PACKAGE_VERSION --acl public-read
 
 echo "Invalidating cache"
 aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_DISTRIBUTION_ID --paths "/*" # TODO: tweak invalidation
