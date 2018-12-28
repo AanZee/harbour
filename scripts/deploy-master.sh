583260f277cf8240c79c3c4aa267dd87333978eb
Michael Maat <michael@aanzee.nl>
2018-12-28 15:15:37 +0100

    [HAR-161] Add echo statements

--- scripts/deploy-master.sh
+++ scripts/deploy-master.sh
@@ -1,8 +1,9 @@
 echo "Building website"
-
 JEKYLL_BASE_URL=/0.0.2 npm run build # TODO: remove base url
 
+echo "Deploying to S3"
 aws configure set preview.cloudfront true
 aws s3 cp ./_gh_pages $AWS_BUCKET/0.0.3 --acl public-read --cache-control
 
+echo "Invalidating cache"
 aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_DISTRIBUTION_ID --paths "/*" # TODO: tweak invalidation
