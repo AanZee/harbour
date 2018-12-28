12f37ed0346de59f0acec02309921a62a722c75b
Michael Maat <michael@aanzee.nl>
2018-12-28 15:32:42 +0100

    [HAR-161] Remove flag

--- scripts/deploy-master.sh
+++ scripts/deploy-master.sh
@@ -3,7 +3,7 @@ JEKYLL_BASE_URL=/0.0.2 npm run build # TODO: remove base url
 
 echo "Deploying to S3"
 aws configure set preview.cloudfront true
-aws s3 cp ./_gh_pages $AWS_BUCKET/0.0.3 --acl public-read --cache-control
+aws s3 cp ./_gh_pages $AWS_BUCKET/0.0.3 --acl public-read
 
 echo "Invalidating cache"
 aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_DISTRIBUTION_ID --paths "/*" # TODO: tweak invalidation
