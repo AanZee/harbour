4beb10b3697d5e65fdbd7601d595fa2e2546a748
Michael Maat <michael@aanzee.nl>
2019-01-02 16:36:01 +0100

    [HAR-161] Use copy instead of sync for version

--- scripts/deploy-master-version.sh
+++ scripts/deploy-master-version.sh
@@ -14,12 +14,9 @@ npm install
 echo "Building website"
 JEKYLL_BASE_URL=/$PACKAGE_VERSION npm run build
 
-echo "Creating version folder"
-aws s3api put-object --bucket $AWS_BUCKET --key $PACKAGE_VERSION
-
 echo "Deploying master to version folder"
 aws configure set preview.cloudfront true
-aws s3 sync ./_gh_pages/. s3://$AWS_BUCKET/$PACKAGE_VERSION --acl public-read
+aws s3 cp ./_gh_pages/. s3://$AWS_BUCKET/$PACKAGE_VERSION --recursive --acl public-read
 
 echo "Invalidating cache"
 aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_DISTRIBUTION_ID --paths "/*" # TODO: tweak invalidation
