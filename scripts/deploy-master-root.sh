99089bea4e9251a6cde72c3c58c319eb4290dc38
Michael Maat <michael@aanzee.nl>
2019-01-02 23:50:32 +0100

    [HAR-161] Fix invalidation and remove comment

--- scripts/deploy-master-root.sh
+++ scripts/deploy-master-root.sh
@@ -25,4 +25,4 @@ aws configure set preview.cloudfront true
 aws s3 sync ./_gh_pages/. s3://$AWS_BUCKET --acl public-read
 
 echo "Invalidating cache"
-aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_DISTRIBUTION_ID --paths "/*" # TODO: tweak invalidation
+aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_DISTRIBUTION_ID --paths /*
