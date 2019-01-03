f640e25602f680921b98b12f6cea2b1f17b8164e
Michael Maat <michael@aanzee.nl>
2019-01-03 09:53:21 +0100

    [HAR-161] Change scripts order

--- scripts/test-build.sh
+++ scripts/test-build.sh
@@ -1,11 +1,8 @@
-echo "Installing awscli"
-pip install --user awscli
+echo "Installing bundle"
+bundle install
 
 echo "Installing global gulp"
 npm install -g gulp-cli
-
-echo "Installing bundle"
-bundle install
 
 echo "Installing npm packages"
 npm install
