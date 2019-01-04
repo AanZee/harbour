a52dfd1c7774bc3a4278eb9ed6e7ba04a9879413
Michael Maat <michael@aanzee.nl>
2019-01-04 16:56:57 +0100

    [HAR-265] Add branchname to footer

--- site/_plugins/environment_variables.rb
+++ site/_plugins/environment_variables.rb
@@ -5,7 +5,7 @@ module Jekyll
 
 	def generate(site)
 		site.config['env'] = ENV['JEKYLL_ENV'] || 'development'
-		site.config['baseurl'] = ENV['JEKYLL_BASE_URL'] || ''
+		site.config['branchName'] = ENV['TRAVIS_BRANCH'] || ''
 		site.config['packageVersion'] = ENV['packageVersion'] || 'reset'
 		# Add other environment variables to `site.config` here...
 		Jekyll.logger.debug site.config.to_yaml
