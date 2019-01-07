8bdd5d7ec8ed3ac059a9fffc10ef396ca6c816d4
Michael Maat <michael@aanzee.nl>
2019-01-07 11:45:38 +0100

    [HAR-265] Add baseurl variable again

--- site/_plugins/environment_variables.rb
+++ site/_plugins/environment_variables.rb
@@ -5,6 +5,7 @@ module Jekyll
 
 	def generate(site)
 		site.config['env'] = ENV['JEKYLL_ENV'] || 'development'
+		site.config['baseurl'] = ENV['JEKYLL_BASE_URL'] || ''
 		site.config['branchName'] = ENV['TRAVIS_BRANCH'] || 'local environment'
 		site.config['packageVersion'] = ENV['packageVersion'] || ''
 		# Add other environment variables to `site.config` here...
